8528f4c1c80e80999175bec24fe9ba67458369fd
Aceticia <liuchris34@gmail.com>
2022-06-21 17:47:30 -0400

    Adding preprocessing of hidden states

--- src/HiddenState.py
+++ src/HiddenState.py
@@ -1,9 +1,7 @@
-import torch
 from src.Cacheable import Cacheable
+import torch
 from pathlib import Path
-
 from typing import Bool
-
 import logging
 logging.getLogger(__name__)
 
@@ -19,20 +17,34 @@ class HiddenState(Cacheable):
         self.network_name = network_name
         self.sample_id = sample_id
 
+        # Preprocess the tensor for storage
+        hiddens = self.preprocess(hiddens)
+
         # Initialize parent class
         super().__init__(
             cache_path=cache_path,
             item_name=f"hiddens_{network_name}_{sample_id}",
             item=hiddens
         )
+    logging.debug(f"Initialized {str(self)}.")
+
+    @staticmethod
+    def preprocess(data: torch.Tensor) -> torch.Tensor:
+        data = data.detach()
+        data = data.to('cpu')
+        return data
 
     @property
     def hiddens(self) -> torch.Tensor:
-        logging.debug(f"Loading hidden state of network {self.network_name} for sample {sample_id}.")
+        logging.debug(f"Loading hidden state of {str(self)}.")
         return super().item
 
     @item.setter
     def hiddens(self, new_hidden: torch.Tensor) -> None:
-        logging.debug(f"Updating hidden state of network {self.network_name} for sample {sample_id}.")
+        logging.debug(f"Updating hidden state of {str(self)}.")
+        new_hidden = self.preprocess(new_hidden)
         super().item = new_hidden
+
+    def __repr__(self):
+        return f"HiddenState(net={self.network_name}, sample={self.sample_id})"
 
