41524641e425cd27e4adae713834d58fd6e13527
Aceticia <liuchris34@gmail.com>
2022-06-23 18:42:58 -0400

    adding test for caching hiddens

--- tests/test_hiddenstate.py
+++ tests/test_hiddenstate.py
@@ -16,20 +16,55 @@ from src.HiddenState import HiddenState
 )
 @pytest.mark.parametrize("hidden_id", list(range(3)))
 @pytest.mark.parametrize("network_name", [f"network{i}" for i in range(3)])
-def test_tensor_w_grad(tmp_path, hidden, hidden_id, network_name):
-    # Create gradient in hidden
-    hidden.requires_grad = True
-    hidden = hidden * torch.randn(
-        1,
-    )
+class TestData:
+    def test_tensor_w_grad(self, tmp_path, hidden, hidden_id, network_name):
+        hidden = hidden.clone()
 
-    # Initialize hidden object
-    HiddenState(
-        cache_path=tmp_path,
-        network_name=network_name,
-        sample_id=hidden_id,
-        hidden=hidden,
-    )
+        # Create gradient in hidden
+        hidden.requires_grad = True
+        hidden = hidden * torch.randn(
+            1,
+        )
 
+        # Initialize hidden object
+        HiddenState(
+            cache_path=tmp_path,
+            network_name=network_name,
+            sample_id=hidden_id,
+            hidden=hidden,
+        )
 
-# TODO: What else to test hidden state? Currently not much is different from cacheable.
+    def test_tensor_is_cached(self, tmp_path, hidden, hidden_id, network_name):
+        hidden = hidden.clone()
+
+        # Initialize hidden object
+        HiddenState(
+            cache_path=tmp_path,
+            network_name=network_name,
+            sample_id=hidden_id,
+            hidden=hidden,
+        )
+
+        assert HiddenState.is_cached(
+            cache_path=tmp_path,
+            network_name=network_name,
+            sample_id=hidden_id
+        )
+
+    def test_tensor_is_not_cached(self, tmp_path, hidden, hidden_id, network_name):
+        hidden = hidden.clone()
+
+        # Initialize hidden object
+        HiddenState(
+            cache_path=tmp_path,
+            network_name=network_name,
+            sample_id=hidden_id,
+            hidden=hidden,
+            load_cached_hidden=False
+        )
+
+        assert not HiddenState.is_cached(
+            cache_path=tmp_path,
+            network_name=network_name,
+            sample_id=hidden_id
+        )
